c95a43725773bbf5cab78f46114e35229e0a6334
Abel Mettzner <mettznermello@gmail.com>
2026-07-06 20:41:42 -0300

    2.0º

--- index.js
+++ index.js
@@ -1,21 +1,38 @@
-import express from "express";
+import express from "express"
+import bancoDeDados from "./repository/index.js"
  
-const app = express();
+const app = express()
  
 //Configurr uma rota de metodo HTTP GET na raiz ("/") do aplicativo
-app.get("/api/v1/somar", (req, res) => {
-    console.log(req.query)
-const num1 = Number(req.query.num1) || 0;
-const num2 = Number(req.query.batata) || 0;
-const resultado = num1 + num2
+//app.get("/api/v1/somar", (req, res) => {
+//    console.log(req.query)
+//const num1 = Number(req.query.num1) || 0;
+//const num2 = Number(req.query.batata) || 0;
+//const resultado = num1 + num2
     //Retorna uma resposta JSON contendo uma mensagem "Hello World"
    
-    res.send({ resultado });
-})
+//    res.send({ resultado });
+//})
 //Inicia o servodor Express na  porta 3000 e define uma função de retorno de chamada
- 
+app.get("/api/pessoa/:id" , (req, res) => {
+    const id = req.params.id
+
+    const pessoa = bancoDeDados.find(it => it.id == id)
+
+    res.send({ pessoa: pessoa})
+})
+
+app.get("/api/pessoa", (req, res) => {
+    //const nome = req.query.name
+    //const id = req.query.name
+    const { id, name } = req.query
+    bancoDeDados.push({id, name})
+    console.log(bancoDeDados)
+
+    res.send({ massege: "Pessoa criada com susseco"})
+})
+
 app.listen(3000, () => {
     //Exibe uma mensagem no console quando o servidor estiver ativo
-   
-    console.log("serve is running in port 3000")
+    console.log("The server is listening to door 3000")
 });
---
+++ repository/index.js
@@ -0,0 +1,12 @@
+//uma const é um espaço na memoria
+const bancoDeDados = []
+//um array é varios espaços na memoria
+
+//bancoDeDados.push("joao")
+// [ "joao" ]
+//bancoDeDados.push("pedro")
+//
+//bancoDeDados.push("guilherme")
+// [ "joao", "pedro", "guilherme" ]
+
+export default bancoDeDados
