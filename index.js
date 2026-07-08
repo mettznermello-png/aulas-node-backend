0e81850b40a6e4fb9e8979ef28ca98c5a4348767
Abel Mettzner <mettznermello@gmail.com>
2026-07-08 20:46:00 -0300

    3.0.2º operaçoes

--- index.js
+++ index.js
@@ -1,47 +1,44 @@
 import express from "express"
-import bancoDeDados from "./repository/index.js"
  
 const app = express()
- 
-//Configurr uma rota de metodo HTTP GET na raiz ("/") do aplicativo
-//app.get("/api/v1/somar", (req, res) => {
-//    console.log(req.query)
-//const num1 = Number(req.query.num1) || 0;
-//const num2 = Number(req.query.batata) || 0;
-//const resultado = num1 + num2
-    //Retorna uma resposta JSON contendo uma mensagem "Hello World"
-   
-//    res.send({ resultado });
-//})
-//Inicia o servodor Express na  porta 3000 e define uma função de retorno de chamada
-app.get("/api/pessoa/:id" , (req, res) => {
-    const id = req.params.id
-    const pessoa = bancoDeDados.find(it => it.id == id )
-    if (!pessoa) {
-        res.status(404).send({ message: "Pessoa não encontrada"})
-        return
-    }
-    res.send({ pessoa: "Pessoa encontrada com sucesso" })
+
+//somar
+app.get("/api/v1/somar", (req, res) => {
+    const {num1, num2 } = req.query
+    const resultado = Number(num1) + Number(num2)
+    
+    res.status(200).send({
+         massege: resultado 
+    })
 })
-app.get("/api/pessoa", (req, res) => {
-    //const nome = req.query.name
-    //const id = req.query.name
-    const { id, name } = req.query
-    if(!id || !name) {
-        res.send({ message: "Favor informar id e name"})
-        return
-    }
-    bancoDeDados.push({id, name})
-    res.send({ massege: "Pessoa criada com susseco"})
+//subtrair
+app.get("/api/v1/subtrair", (req, res) => {
+    const {num1, num2 } = req.query
+    const resultado = Number(num1) - Number(num2)
+    
+    res.status(200).send({
+         massege: resultado 
+    })
 })
 
-app.get("/api/pessoa/alterar/:id", (req, req) => {
-    let id = req.params
-    let name = 
+//multi
+app.get("/api/v1/multiplicar", (req, res) => {
+    const {num1, num2 } = req.query
+    const resultado = Number(num1) * Number(num2)
+    
+    res.status(200).send({
+         massege: resultado 
+    })
 })
 
-app.get("/api/pessoa/deletar/:id", (req, req) => {
-    const id = 
+//div
+app.get("/api/v1/dividir", (req, res) => {
+    const { num1, num2} = req.query
+    const resultado = Number(num1) / Number(num2)
+
+    res
+        .status(200)
+        .send({ massege: resultado})
 })
 
 app.listen(3000, () => {
