d23f905ca85d98f6cd8fedb11363193e859508d7
Abel Mettzner <mettznermello@gmail.com>
2026-07-08 20:47:27 -0300

    concerto sem operaçoes

--- index.js
+++ index.js
@@ -2,44 +2,6 @@ import express from "express"
  
 const app = express()
 
-//somar
-app.get("/api/v1/somar", (req, res) => {
-    const {num1, num2 } = req.query
-    const resultado = Number(num1) + Number(num2)
-    
-    res.status(200).send({
-         massege: resultado 
-    })
-})
-//subtrair
-app.get("/api/v1/subtrair", (req, res) => {
-    const {num1, num2 } = req.query
-    const resultado = Number(num1) - Number(num2)
-    
-    res.status(200).send({
-         massege: resultado 
-    })
-})
-
-//multi
-app.get("/api/v1/multiplicar", (req, res) => {
-    const {num1, num2 } = req.query
-    const resultado = Number(num1) * Number(num2)
-    
-    res.status(200).send({
-         massege: resultado 
-    })
-})
-
-//div
-app.get("/api/v1/dividir", (req, res) => {
-    const { num1, num2} = req.query
-    const resultado = Number(num1) / Number(num2)
-
-    res
-        .status(200)
-        .send({ massege: resultado})
-})
 
 app.listen(3000, () => {
     //Exibe uma mensagem no console quando o servidor estiver ativo
