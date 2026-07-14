42f53189d7b1c8ccc156037d2704d713e8457516
Abel Mettzner <mettznermello@gmail.com>
2026-07-13 21:30:36 -0300

    aula 3

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
 import express from "express"
-import bancoDeDados from "./repository/index.js"
  
 const app = express()
+
+app.use(express.json())
  
+app.post("/api/v1/somar", (req, res) => {
+    const { num1, num2 } = req.body
+    const resultado = Number(num1) + Number(num2)
+
+    res.status(200).send({ massage: resultado })
+    return
+})
+
+app.post("/api/v1/subtrair", (req, res) => {
+    const { num1, num2 } = req.body
+    const resultado = Number(num1) - Number(num2)
+
+    res.status(200).send({ massage: resultado })
+    return
+})
+
+app.post("/api/v1/multi", (req, res) => {
+    const { num1, num2 } = req.body
+    const resultado = Number(num1) * Number(num2)
+
+    res.status(200).send({ massage: resultado })
+    return
+})
+
+app.post("/api/v1/dividir", (req, res) => {
+    const { num1, num2 } = req.body
+    const resultado = Number(num1) / Number(num2)
+
+    res.status(200).send({ massage: resultado })
+})
+
 
 
 app.listen(3000, () => {
     //Exibe uma mensagem no console quando o servidor estiver ativo
     console.log("The server is listening to door 3000")
-});
+})
